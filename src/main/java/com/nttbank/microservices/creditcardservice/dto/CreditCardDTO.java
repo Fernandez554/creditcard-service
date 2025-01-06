9c3a79dc389be7ddb748ccc88c8be2be1aac9f56
Jesus Alberto Fernandez Malpartida <jfernmal@emeal.nttdata.com>
2025-01-06 02:28:13 -0500

    Fix: SonarQube-reported issues

--- src/main/java/com/nttbank/microservices/creditcardservice/dto/CreditCardDTO.java
+++ src/main/java/com/nttbank/microservices/creditcardservice/dto/CreditCardDTO.java
@@ -6,14 +6,10 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 /**
- * Represents a credit card with various attributes including credit card ID, customer ID, status,
- * credit card number, expiration date, CVV, credit limit, and available credit. This class is used
- * to interact with the 'credit_cards' collection in the MongoDB database.
+ * CreditCardDTO class that represents the credit card collection.
  */
 @Data
 @AllArgsConstructor
@@ -25,18 +21,14 @@ public class CreditCardDTO {
 
   @NotNull(message = "Customer Identifier cannot be null")
   private String customerId;
-
   private String cardType;
-
   @NotNull(message = "Credit Limit cannot be null")
   private BigDecimal creditLimit;
   private BigDecimal currentBalance;
   @NotNull(message = "Expiration Date cannot be null")
   private LocalDate expirationDate;
-
   @NotNull(message = "Issue Statement Day cannot be null")
   private String issueStatementDay;
-
   @NotNull(message = "Maintenance Fee cannot be null")
   private BigDecimal maintenanceFee;
   private String status;
